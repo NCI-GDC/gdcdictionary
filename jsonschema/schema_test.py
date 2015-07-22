1805f9b051714034e5aa6c85e1220515c9c700b4
Joshua Miller <jshuasmiller@gmail.com>
2015-07-22 15:41:39 -0500

    Add case example.
    - Adds case example
    - Adds some docstrings
    - Cleans up example script

--- jsonschema/schema_test.py
+++ jsonschema/schema_test.py
@@ -1,8 +1,18 @@
+"""This is an example of json schema for the GDC using schemas defined
+in local yaml files.
+
+Included are a few functions to augment jsonschema and the python
+validator.
+
+Examples are at the end.
+
+"""
+
+
 from jsonschema import validate, RefResolver
-import yaml
-import uuid
 import copy
-from pprint import pprint
+import uuid
+import yaml
 
 
 def load_yaml_schema(path):
@@ -11,7 +21,9 @@ def load_yaml_schema(path):
 
 
 def merge_schemas(a, b, path=None):
-    "merges b into a"
+    """Recursively zip schemas together
+
+    """
     path = path if path is not None else []
     for key in b:
         if key in a:
@@ -30,8 +42,12 @@ def merge_schemas(a, b, path=None):
     return a
 
 
-def get_project_specific_schema(project, entity_type):
-    root = copy.deepcopy(schema[entity_type])
+def get_project_specific_schema(projects, project, schema, entity_type):
+    """Look up the core schema for its type and override it with any
+    project level overrides
+
+    """
+    root = copy.deepcopy(schema)
     project_overrides = projects.get(project)
     if project_overrides:
         overrides = project_overrides.get(entity_type)
@@ -40,30 +56,70 @@ def get_project_specific_schema(project, entity_type):
     return root
 
 
+def validate_entity(entity, schemata, resolver=None, project=None):
+    """Validate an entity by looking up the core schema for its type and
+    overriding it with any project level overrides
+
+    """
+    local_schema = get_project_specific_schema(
+        projects, project, schemata[entity['type']], entity['type'])
+    return validate(entity, local_schema, resolver=resolver)
+
+
 if __name__ == '__main__':
+
+    ####################
+    # Setup
+    ####################
 
     # Load schemata
     metaschema = load_yaml_schema('metaschema.yaml')
     definitions = load_yaml_schema('definitions.yaml')
-    schema = load_yaml_schema('schema.yaml')
-    project1 = load_yaml_schema('project1.yaml')
+    project1 = load_yaml_schema('projects/project1.yaml')
     projects = {'project1': project1}
     resolver = RefResolver('definitions.yaml#', definitions)
 
-    # validate aliquot schema
-    validate(schema['aliquot'], metaschema['entity'])
+    schemata = {
+        "aliquot": load_yaml_schema('aliquot.yaml'),
+        "case": load_yaml_schema('case.yaml'),
+    }
 
-    # # Example document
-    # doc = {
-    #     "type": "aliquot",
-    #     "submitter_aliquot_id": 'aliquot_1',
-    #     "parents": {
-    #         "analyte_ids": [str(uuid.uuid4())]
-    #     },
-    #     "project_1_specific_thing": 'test string',
-    # }
+    # validate schemata
+    map(lambda s: validate(s, metaschema['entity']), schemata.values())
 
-    # # Example validation
-    # local_schema = get_project_specific_schema('project1', doc['type'])
-    # validate(doc, local_schema, resolver=resolver)
-    # print('\nsuccess.')
+    ####################
+    # Example validation
+    ####################
+
+    aliquot1 = {
+        "type": "aliquot",
+        "submitter_aliquot_id": 'aliquot_1',
+        "parents": {
+            "analyte_ids": [str(uuid.uuid4())]
+        },
+        "project_1_specific_thing": 'test string',
+    }
+
+    case1 = {
+        "type": "case",
+        "submitter_case_id": 'case_1',
+        "parents": {
+            "project_id": str(uuid.uuid4())
+        },
+        "gender": "female",
+        "race": "Unknown"
+    }
+
+    # These should pass
+    validate_entity(case1, schemata, resolver)
+    validate_entity(aliquot1, schemata, resolver, 'project1')
+
+    # These should fail
+    try:
+        validate_entity(aliquot1, schemata, resolver)
+    except:
+        pass
+    else:
+        raise Exception('Expected to fail')
+
+    print('\nsuccess.')
