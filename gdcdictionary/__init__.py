fe371431693ade39f5572efbab6acdee9b2ecec0
philloooo <phillis.tt@gmail.com>
2017-11-30 18:53:24 -0600

    feat(schema): remove python helper and push to s3

--- gdcdictionary/__init__.py
+++ gdcdictionary/__init__.py
@@ -1,1 +1,6 @@
-from python import GDCDictionary, gdcdictionary
+import os
+from dictionaryutils import DataDictionary as GDCDictionary
+
+SCHEMA_DIR = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), 'schemas')
+gdcdictionary = GDCDictionary(root_dir=SCHEMA_DIR)
--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version='0.0.0',
     packages=find_packages(),
     install_requires=[
-        'PyYAML==3.11',
-        'jsonschema==2.5.1',
+        'dictionaryutils',
+    ],
+    dependency_links=[
+       "git+https://github.com/uc-cdis/dictionaryutils.git@1.2.0#egg=dictionaryutils",
     ],
     package_data={
         "gdcdictionary": [
