74e76fafa0e046768b39d5e04f60becb0f6588ed
Justin Barnowski <jbarno@uchicago.edu>
2019-03-11 11:49:02 -0500

    feat(tests): tests are parametrized and invalid failing

--- tests/json_validation_test.py
+++ tests/json_validation_test.py
@@ -8,57 +8,73 @@ to it
 
 """
 
-
+from utils import DATA_DIR
 from jsonschema import validate, ValidationError
+
+import json
 import glob
 import os
-import json
-from gdcdictionary import ROOT_DIR
+import pytest
 
-from utils import validate_entity, BaseTest
-
-DATA_DIR = os.path.join(ROOT_DIR, 'examples')
-
-class JsonValidationTests(BaseTest):
-
-    def test_valid_files(self):
-        for path in glob.glob(os.path.join(DATA_DIR, 'valid', '*.json')):
-            print "Validating {}".format(path)
-            doc = json.load(open(path, 'r'))
-            print(doc)
-            if type(doc) == dict:
-                self.add_system_props(doc)
-                validate_entity(doc, self.dictionary.schema)
-            elif type(doc) == list:
+def get_all_docs(subdir):
+    for path in glob.glob(os.path.join(DATA_DIR, subdir, '*.json')):
+        with open(path, 'r') as f:
+            doc = json.load(f)
+            if isinstance(doc, dict):
+                yield doc
+            elif isinstance(doc, list):
                 for entity in doc:
-                    self.add_system_props(entity)
-                    validate_entity(entity, self.dictionary.schema)
+                    yield entity
             else:
-                raise Exception("Invalid json")
+                assert False, "Error collecting file: {}".format(f)
 
-    def test_invalid_files(self):
-        for path in glob.glob(os.path.join(DATA_DIR, 'invalid', '*.json')):
-            print "Validating {}".format(path)
-            doc = json.load(open(path, 'r'))
-            if type(doc) == dict:
-                self.add_system_props(doc)
-                with self.assertRaises(ValidationError):
-                    validate_entity(doc, self.dictionary.schema)
-            elif type(doc) == list:
-                for entity in doc:
-                    self.add_system_props(entity)
-                    with self.assertRaises(ValidationError):
-                        validate_entity(entity, self.dictionary.schema)
-            else:
-                raise Exception("Invalid json")
 
-    def add_system_props(self, doc):
-        schema = self.dictionary.schema[doc['type']]
-        for key in schema['systemProperties']:
-            use_def_default = (
-                '$ref' in schema['properties'][key] and
-                key in self.definitions and
-                'default' in self.definitions[key]
-            )
-            if use_def_default:
-                doc[key] = self.definitions[key]['default']
+@pytest.mark.parametrize("doc", get_all_docs("valid"))
+def test_valid_examples(doc, schema):
+    validate(doc,schema)
+
+@pytest.mark.parametrize("doc", get_all_docs("invalid"))
+def test_invalid_examples(doc, schema):
+    with pytest.raises(ValidationError):
+        validate(doc, schema)
+
+#class JsonValidationTests(BaseTest):
+#
+#    def test_valid_files(self):
+#        for path in glob.glob(os.path.join(DATA_DIR, 'valid', '*.json')):
+#            if type(doc) == dict:
+#                self.add_system_props(doc)
+#            elif type(doc) == list:
+#                for entity in doc:
+#                    self.add_system_props(entity)
+#                    validate_entity(entity, self.dictionary.schema)
+#            else:
+#                raise Exception("Invalid json")
+#
+#    def test_invalid_files(self):
+#        for path in glob.glob(os.path.join(DATA_DIR, 'invalid', '*.json')):
+#            print "Validating {}".format(path)
+#            doc = json.load(open(path, 'r'))
+#            if type(doc) == dict:
+#                self.add_system_props(doc)
+#                with self.assertRaises(ValidationError):
+#                    validate_entity(doc, self.dictionary.schema)
+#            elif type(doc) == list:
+#                for entity in doc:
+#                    self.add_system_props(entity)
+#                    with self.assertRaises(ValidationError):
+#                        validate_entity(entity, self.dictionary.schema)
+#            else:
+#                raise Exception("Invalid json")
+#
+#    def add_system_props(self, doc):
+#        schema = self.dictionary.schema[doc['type']]
+#        for key in schema['systemProperties']:
+#            use_def_default = (
+#                '$ref' in schema['properties'][key] and
+#                key in self.definitions and
+#                'default' in self.definitions[key]
+#            )
+#            if use_def_default:
+#                doc[key] = self.definitions[key]['default']
+#
