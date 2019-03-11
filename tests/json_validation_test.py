f14542d8ea4746220abd1709b7fe977abc879f37
Justin Barnowski <jbarno@uchicago.edu>
2019-03-11 12:00:14 -0500

    fix(schema): compare against the specific schema
    deleted commented code

--- tests/json_validation_test.py
+++ tests/json_validation_test.py
@@ -31,50 +31,9 @@ def get_all_docs(subdir):
 
 @pytest.mark.parametrize("doc", get_all_docs("valid"))
 def test_valid_examples(doc, schema):
-    validate(doc,schema)
+    validate(doc, schema[doc["type"]])
 
 @pytest.mark.parametrize("doc", get_all_docs("invalid"))
 def test_invalid_examples(doc, schema):
     with pytest.raises(ValidationError):
-        validate(doc, schema)
-
-#class JsonValidationTests(BaseTest):
-#
-#    def test_valid_files(self):
-#        for path in glob.glob(os.path.join(DATA_DIR, 'valid', '*.json')):
-#            if type(doc) == dict:
-#                self.add_system_props(doc)
-#            elif type(doc) == list:
-#                for entity in doc:
-#                    self.add_system_props(entity)
-#                    validate_entity(entity, self.dictionary.schema)
-#            else:
-#                raise Exception("Invalid json")
-#
-#    def test_invalid_files(self):
-#        for path in glob.glob(os.path.join(DATA_DIR, 'invalid', '*.json')):
-#            print "Validating {}".format(path)
-#            doc = json.load(open(path, 'r'))
-#            if type(doc) == dict:
-#                self.add_system_props(doc)
-#                with self.assertRaises(ValidationError):
-#                    validate_entity(doc, self.dictionary.schema)
-#            elif type(doc) == list:
-#                for entity in doc:
-#                    self.add_system_props(entity)
-#                    with self.assertRaises(ValidationError):
-#                        validate_entity(entity, self.dictionary.schema)
-#            else:
-#                raise Exception("Invalid json")
-#
-#    def add_system_props(self, doc):
-#        schema = self.dictionary.schema[doc['type']]
-#        for key in schema['systemProperties']:
-#            use_def_default = (
-#                '$ref' in schema['properties'][key] and
-#                key in self.definitions and
-#                'default' in self.definitions[key]
-#            )
-#            if use_def_default:
-#                doc[key] = self.definitions[key]['default']
-#
+        validate(doc, schema[doc["type"]])
