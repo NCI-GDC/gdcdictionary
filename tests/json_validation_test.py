bebf706cf6db57f37d485ae6186bff31115e7977
Justin Barnowski <jbarno@uchicago.edu>
2019-03-11 12:16:23 -0500

    feat(id): create ids for paremterization
    Let the test output which file is borked

--- tests/json_validation_test.py
+++ tests/json_validation_test.py
@@ -16,24 +16,34 @@ import glob
 import os
 import pytest
 
+
 def get_all_docs(subdir):
-    for path in glob.glob(os.path.join(DATA_DIR, subdir, '*.json')):
+    for path in sorted(glob.glob(os.path.join(DATA_DIR, subdir, '*.json'))):
         with open(path, 'r') as f:
             doc = json.load(f)
             if isinstance(doc, dict):
                 yield doc
             elif isinstance(doc, list):
+                if len(doc) != 1:
+                    assert False, "Mismatch between files and documents in {}".format(f.name)
                 for entity in doc:
                     yield entity
             else:
                 assert False, "Error collecting file: {}".format(f)
 
+def get_all_files(subdir):
+    file_names = []
+    for path in sorted(glob.glob(os.path.join(DATA_DIR, subdir, '*.json'))):
+        with open(path, 'r') as f:
+            file_names.append(f.name)
+    return file_names
 
-@pytest.mark.parametrize("doc", get_all_docs("valid"))
+
+@pytest.mark.parametrize("doc", get_all_docs("valid"), ids=get_all_files("valid"))
 def test_valid_examples(doc, schema):
     validate(doc, schema[doc["type"]])
 
-@pytest.mark.parametrize("doc", get_all_docs("invalid"))
+@pytest.mark.parametrize("doc", get_all_docs("invalid"), ids=get_all_files("invalid"))
 def test_invalid_examples(doc, schema):
     with pytest.raises(ValidationError):
         validate(doc, schema[doc["type"]])
