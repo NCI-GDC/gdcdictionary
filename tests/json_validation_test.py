6b5d4c513b8403b0a9766ffaf94b6ad8452fb7bc
Justin Barnowski <jbarno@uchicago.edu>
2019-03-12 09:18:05 -0500

    refactor(simplify): just pass path to tests solves the id confusion

--- tests/json_validation_test.py
+++ tests/json_validation_test.py
@@ -17,33 +17,19 @@ import os
 import pytest
 
 
-def get_all_docs(subdir):
+def get_all_paths(subdir):
     for path in sorted(glob.glob(os.path.join(DATA_DIR, subdir, '*.json'))):
-        with open(path, 'r') as f:
-            doc = json.load(f)
-            if isinstance(doc, dict):
-                yield doc
-            elif isinstance(doc, list):
-                if len(doc) != 1:
-                    assert False, "Mismatch between files and documents in {}".format(f.name)
-                for entity in doc:
-                    yield entity
-            else:
-                assert False, "Error collecting file: {}".format(f)
+        yield path
 
-def get_all_files(subdir):
-    file_names = []
-    for path in sorted(glob.glob(os.path.join(DATA_DIR, subdir, '*.json'))):
-        with open(path, 'r') as f:
-            file_names.append(f.name)
-    return file_names
-
-
-@pytest.mark.parametrize("doc", get_all_docs("valid"), ids=get_all_files("valid"))
-def test_valid_examples(doc, schema):
-    validate(doc, schema[doc["type"]])
-
-@pytest.mark.parametrize("doc", get_all_docs("invalid"), ids=get_all_files("invalid"))
-def test_invalid_examples(doc, schema):
-    with pytest.raises(ValidationError):
+@pytest.mark.parametrize("path", get_all_paths("valid"))
+def test_valid_examples(path, schema):
+    with open(path, 'r') as f:
+        doc = json.load(f)
         validate(doc, schema[doc["type"]])
+
+@pytest.mark.parametrize("path", get_all_paths("invalid"))
+def test_invalid_examples(path, schema):
+    with open(path, 'r') as f:
+        doc = json.load(f)
+        with pytest.raises(ValidationError):
+            validate(doc, schema[doc["type"]])
