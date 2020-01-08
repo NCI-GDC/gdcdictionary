0aadfec2d84911f8f0225a0e00165a16ceda1f85
Rowland Ogwara <rogwara@uchicago.edu>
2020-01-08 09:54:17 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gdcdictionary',
-    version="2.0.0-a1",
+    version="2.0.0",
     packages=find_packages(),
     install_requires=[
         'PyYAML',
