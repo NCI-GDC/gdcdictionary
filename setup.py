cc81ac9ff9c1be880a35710c8c33d897b4da2f73
Arthur Collet <acollet@uchicago.edu>
2022-10-20 11:44:34 -0500

    Recent versions of setuptools_scm break with previous git tags

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         'local_scheme': 'dirty-tag',
         'write_to': 'gdcdictionary/_version.py',
     },
-    setup_requires=['setuptools_scm<6'],
+    setup_requires=['setuptools_scm==3.5.0'],
     install_requires=[
         'PyYAML>=5.1.2,<5.5',
         'jsonschema>=3.0.2,<3.3',
